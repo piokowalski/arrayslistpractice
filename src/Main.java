c86090d35aaf8a36ab94e4378f05788aecf79aaf
piokowalski <piokowalski@onet.pl>
2019-04-07 21:03:08 +0200

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> words = new ArrayList<String>();
+        words.add("hello");
+        words.add("academy");
+        words.add("java");
+        words.add("junior");
+
+
+
+    }
+}
