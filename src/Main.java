338cb780595e51b78ffc2fd8fbe458e74595d9ba
piokowalski <piokowalski@onet.pl>
2019-04-07 21:20:42 +0200

    Stream created

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<String> words = new ArrayList<String>();
-        words.add("hello");
-        words.add("academy");
-        words.add("java");
-        words.add("junior");
+        List<String> words = Arrays.asList(
+                new String("hello");
+                new String("academy");
+                new String("java");
+                new String("junior");
+        );
+
+       words.stream()
+               .
+
+
 
     }
 }
